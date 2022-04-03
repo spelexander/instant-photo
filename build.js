b9b0d5808db2521bdc2de7342c41ff59a7b2a07c
spelexander <spelexander93@gmail.com>
2022-04-03 23:51:36 +1000

    Fixing build and publish

---
+++ build.js
@@ -0,0 +1,26 @@
+const { build } = require("esbuild");
+const { dependencies } = require("./package.json");
+
+const entryFile = "src/instant-photo.tsx";
+const shared = {
+    bundle: true,
+    entryPoints: [entryFile],
+    external: Object.keys(dependencies),
+    logLevel: "info",
+    minify: true,
+    sourcemap: true,
+};
+
+build({
+    ...shared,
+    format: "esm",
+    outfile: "./dist/index.esm.js",
+    target: ["esnext", "node12.22.0"],
+});
+
+build({
+    ...shared,
+    format: "cjs",
+    outfile: "./dist/index.cjs.js",
+    target: ["esnext", "node12.22.0"],
+});
